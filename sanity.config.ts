febad7d47dcc6283eed3a6959c78049ae728ec20
celestialord01-eng <celestialord01@gmail.com>
2026-05-14 11:08:17 +0530

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,1 +1,18 @@
 
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {schemaTypes} from './sanity/schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'VelvetBlog',
+
+  projectId: '35jhn74x',
+  dataset: 'production',
+
+  plugins: [deskTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})

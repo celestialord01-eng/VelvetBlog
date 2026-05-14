7b7fbd723bed26412aeaaa8e1211b596a5b27333
celestialord01-eng <celestialord01@gmail.com>
2026-05-14 10:09:01 +0530

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,1 +1,12 @@
 
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://velvet-blog.vercel.app/sitemap.xml',
+  }
+}
